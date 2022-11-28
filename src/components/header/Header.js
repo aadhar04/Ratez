cd1a8e41c26e68bb8967455a71ae4ca80d48c448
Aadhar Aggarwal <aadharoct@gmail.com>
2022-11-29 00:29:58 +0530

    change commit

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import img from "./Images/imgg.jpg";
 
 const Header = () => {
   return (
@@ -9,7 +10,7 @@ const Header = () => {
         <Link to="/">
           <img
             className="header_icon"
-            src="https://api2.iloveimg.com/v1/download/pg9hnc72ggkxxdfqA4jA6l1mhdzhr7xfh2vjxq647g5rb367wdsvvx76wphhjrfyh3xbvm6pll0k25l16kb5rtxtgv70djywjk3wggclrw03ch16z51jfqv0y1qxl6rdcsy4d3A9pd8z605A3k88zjpblb91tvrm29dAAw4xrtw0sh6p9pgq"
+            src={img}
           />
         </Link>
         <Link to="/movies/popular" style={{ textDecoration: "none" }}>
